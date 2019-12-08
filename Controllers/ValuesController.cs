7ee8e605d7f16ec50063ff78172e4f998dd98c65
burak.caglayan@outlook.com <burak.caglayan@outlook.com>
2019-12-08 17:59:44 +0300

    first commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace Ddd_UoW_Sample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly CircuitBreakerPolicy<PingResponse> _breakerPolicy;
+        private readonly HttpClient _httpClient;
+        ElasticClient client;
+        public ValuesController(CircuitBreakerPolicy<PingResponse> breakerPolicy)
+        {
+            _breakerPolicy = breakerPolicy;
+            var configuration = new ConnectionSettings(new Uri("http://localhost:9200")).DefaultIndex("product");
+            client = new ElasticClient(configuration);
+        }
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var inventoryResponse = _breakerPolicy.Execute(() => client.Ping());
+            System.Console.WriteLine($"Elastic status: {inventoryResponse.IsValid}");
+
+            return StatusCode((inventoryResponse.IsValid ? (int)HttpStatusCode.OK : (int)HttpStatusCode.InternalServerError), "hata var");
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Nest;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+
+namespace Ddd_UoW_Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            CircuitBreakerPolicy<PingResponse> breakerPolicy = Polly.Policy
+            .HandleResult<PingResponse>(r => !r.IsValid)
+            .CircuitBreaker(2, TimeSpan.FromSeconds(5), OnBreak, OnReset, onHalfOpen: OnHalfOpen);
+
+            services.AddSingleton<CircuitBreakerPolicy<PingResponse>>(breakerPolicy);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
+            }
+
+            // app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private void OnHalfOpen()
+        {
+            Console.WriteLine("\t\t\t\t\tConnection half open");
+        }
+
+        private void OnReset(Polly.Context context)
+        {
+            Console.WriteLine("\t\t\t\t\tConnection reset");
+        }
+
+        private void OnBreak(DelegateResult<PingResponse> delegateResult, TimeSpan timeSpan, Polly.Context context)
+        {
+            Console.WriteLine($"\t\t\t\t\tConnection break: {delegateResult.Result.IsValid}");
+        }
+    }
+}
